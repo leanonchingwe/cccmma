bfc080329fc1160bbaac7ead06b3255e46d786c1
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2023-10-06 16:32:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
-
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
@@ -10,51 +9,35 @@ import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
   return (
-    <Layout className="full-width">
+    <Layout className="container">
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
-      <main className="w-full diplay:flex">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
+      <main className="row">
+        <div className="col-lg-12 text-center mb-5">
+          <h1 className="display-4">{globalData.blogTitle}</h1>
+        </div>
+        <ul className="list-unstyled w-100">
           {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+            <li key={post.filePath} className="border-bottom mb-4 pb-4">
+              <Link as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`} href={`/posts/[slug]`}>
+                <a className="d-block mb-2">
                   {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
+                    <p className="text-uppercase mb-0 font-weight-bold opacity-60">{post.data.date}</p>
                   )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                  <h2 className="h4">{post.data.title}</h2>
                   {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
+                    <p className="mt-2 mb-0 text-muted">{post.data.description}</p>
                   )}
-                  <ArrowIcon className="mt-4" />
                 </a>
               </Link>
+              <ArrowIcon className="mt-2" />
             </li>
           ))}
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
+      <GradientBackground variant="large" className="fixed top-20 opacity-40" />
+      <GradientBackground variant="small" className="absolute bottom-0 opacity-20" />
     </Layout>
   );
 }
