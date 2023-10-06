36a40190220cef499858cec35247dbcc7460409c
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2023-10-06 16:24:17 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -51,9 +51,32 @@ export default function Layout({ children }) {
 
   return (
     <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        {children}
-      </div>
+      {/* Financial News Section */}
+      <section className="bg-white py-10">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          <h2 className="text-2xl font-bold mb-6">Financial News Section</h2>
+          {/* Add your financial news content here */}
+        </div>
+      </section>
+
+      {/* Stock Information Section */}
+      <section className="bg-gray-100 py-10">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          <h2 className="text-2xl font-bold mb-6">Stock Information Section</h2>
+          {/* Add your stock information content here */}
+        </div>
+      </section>
+
+      {/* Financial Insights Section */}
+      <section className="bg-white py-10">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          <h2 className="text-2xl font-bold mb-6">Financial Insights Section</h2>
+          {/* Add your financial insights content here */}
+        </div>
+      </section>
+
+      {/* Other Sections */}
+      {children}
     </div>
   );
 }
